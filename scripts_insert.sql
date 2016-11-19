1192874e50381318c8c2dae14fafc527d7368d2b
Unknown <pedro.augusto.tecinf@gmail.com>
2016-11-19 14:07:57 -0400

    atulização 1
    alterações das tabelas e inserções de alguns scripts

---
+++ scripts_insert.sql
@@ -0,0 +1,13 @@
+
+insert into pessoa() values();
+insert into cliente() values();
+insert into funcionario() values();
+insert into medico() values();
+
+insert into categoria() values();
+insert into produtos() values();
+insert into fornecedor() values();
+insert into estoque() values();
+
+insert into carrinho() values();
+insert into venda() values();
